411680eb6d3de11264acbb8394538941b4ed396f
Никита Усанов <usannikita2@gmail.com>
2023-12-17 02:40:19 +0900

    Добавьте файлы проекта.

---
+++ Reshenie 6 variant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reshenie_6_variant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            double x = rand.NextDouble(); 
+
+            double y = rand.NextDouble() * 2 * Math.PI;
+
+            double M = Math.Pow(Math.Cos(y), 2) + 3.6 * Math.Exp(x);
+
+            Console.WriteLine($"Случайное значение x: {x}");
+
+            Console.WriteLine($"Случайное значение y: {y}");
+
+            Console.WriteLine($"Результат M = cos^2(y) + 3.6e^x: {M}");
+        }
+    }
+}
